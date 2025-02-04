8c83453610a5d71e0e28f952d7313bbcd0e6c268
ChristiaanOpperman <christiaan.rudolfo@gmail.com>
2025-02-04 19:08:59 +0200

    ADDED ability to add posts with community logic

--- dumpwatch/frontend/src/components/CreateReportForm.js
+++ dumpwatch/frontend/src/components/CreateReportForm.js
@@ -20,14 +20,27 @@ const CreateReportForm = () => {
     const [fileSize, setFileSize] = useState('');
     const [placeOptions, setPlaceOptions] = useState([]);
     const [placeDetails, setPlaceDetails] = useState([]);
+    const [locationMode, setLocationMode] = useState("community");
+    const [userCommunities, setUserCommunities] = useState([]);
+
+
 
     // get user id from local storage
     const userId = parseInt(localStorage.getItem('userId'));
     const imageInputRef = useRef(null);
 
+    const getUserCommunities = async () => {
+        try {
+            const response = await axios.get(`/get-user-place-details/${userId}`);
+            setUserCommunities(response.data);
+        } catch (error) {
+            console.error('Error fetching user communities:', error);
+        }
+    };
+
     useEffect(() => {
         fetchPlaces();
-
+        getUserCommunities();
     }, []);
 
     useEffect(() => {
@@ -39,18 +52,17 @@ const CreateReportForm = () => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 async (position) => {
-                    const lat = position.coords.latitude.toFixed(4);
-                    const lng = position.coords.longitude.toFixed(4);
+                    const lat = position.coords.latitude.toFixed(8);
+                    const lng = position.coords.longitude.toFixed(8);
                     setLatitude(lat);
                     setLongitude(lng);
-                    console.log('Current location:', lat, lng);
-
                     try {
                         const response = await axios.get(`/get-place-by-coordinates?lat=${lat}&lng=${lng}`);
-                        console.log('Place details:', response.data);
-                        setPlaceId(response.data.placeId);
-                        setPlaceDetailId(response.data.placeDetailId);
-                        setPostalCode(response.data.postalCode);
+                        const placeOptions = response.data.map((place) => ({
+                            value: place.placeId,
+                            label: place.placeName
+                        }));
+                        setPlaceOptions(placeOptions);
                     } catch (error) {
                         console.error('Error fetching place details:', error);
                         setMessage('Could not find details for your location.');
@@ -175,22 +187,93 @@ const CreateReportForm = () => {
                     </div>
                 )}
 
-                <div className="flex items-center justify-center mb-6">
-                    <input
-                        type="checkbox"
-                        id="locationSwitch"
-                        checked={useCurrentLocation}
-                        onChange={() => setUseCurrentLocation(!useCurrentLocation)}
-                        // className="mr-2"
-                        className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-green-700 checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-green-700 checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0"
-
-                    />
-                    <label htmlFor="locationSwitch" className="text-sm font-medium text-gray-900">
-                        {useCurrentLocation ? 'Use Current Location' : 'Manually Enter Address'}
-                    </label>
+                <div className="flex space-x-4">
+                    <button
+                        className={`py-2 px-4 rounded-lg ${locationMode === "community" ? "bg-green-700 text-white" : "bg-gray-300"}`}
+                        onClick={() => setLocationMode("community")}
+                    >
+                        In My Community
+                    </button>
+                    <button
+                        className={`py-2 px-4 rounded-lg ${locationMode === "current" ? "bg-green-700 text-white" : "bg-gray-300"}`}
+                        onClick={() => {
+                            setLocationMode("current")
+                            fetchUserLocation();
+                        }}
+                    >
+                        Use Current Location
+                    </button>
+                    <button
+                        className={`py-2 px-4 rounded-lg ${locationMode === "manual" ? "bg-green-700 text-white" : "bg-gray-300"}`}
+                        onClick={() => {
+                            setLocationMode("manual")
+                            fetchPlaces();
+                        }}
+                    >
+                        Manually Enter Address
+                    </button>
                 </div>
-
-                {!useCurrentLocation && (
+                {locationMode === "current" && (
+                    <>
+                        <div>
+                            <label className="block font-bold mb-5">Places Near You:</label>
+                            <Select
+                                options={placeOptions}
+                                value={placeOptions.find(option => option.value === placeId)}
+                                onChange={(selectedOption) => {
+                                    console.log('Selected option:', selectedOption);
+                                    setPlaceId(selectedOption.value);
+                                    fetchPlaceDetails(selectedOption.value);
+                                }}
+                                placeholder="Select a place"
+                                className="w-full border p-2 rounded-lg"
+                                isSearchable
+                            />
+                        </div>
+                        {placeDetails && placeDetails.length > 0 && (
+                            <div>
+                                <label className="block font-bold mb-1">Postal Code:</label>
+                                <Select
+                                    options={placeDetails.map((detail) => ({ value: detail.placeDetailId, label: detail.postalCode }))}
+                                    value={placeDetails.find((detail) => detail.value === postalCode)}
+                                    onChange={(selectedOption) => {
+                                        setPostalCode(selectedOption.label)
+                                    }
+                                    }
+                                    placeholder="Select a postal code"
+                                    className="w-full border p-2 rounded-lg"
+                                    isSearchable
+                                />
+                            </div>
+                        )}
+                        <button
+                            type="button"
+                            onClick={fetchUserLocation}
+                            className="flex items-center justify-center bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600 transition-all duration-200 disabled:opacity-50 mt-5"
+                            disabled={isRefreshing}
+                        >
+                            <span>Refresh Location</span>
+                            {isRefreshing ? (<div
+                                className="inline-block ml-2 h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                                role="status">
+                                <span
+                                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                                >Loading...</span>
+                            </div>) :
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-4 ml-2"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                </svg>}
+                        </button>
+                    </>
+                )}
+                {locationMode === "manual" && (
                     <div>
                         <label className="block font-bold mb-1">Select Place:</label>
                         <Select
@@ -200,56 +283,41 @@ const CreateReportForm = () => {
                                 setPlaceId(selectedOption.value);
                                 fetchPlaceDetails(selectedOption.value);
                             }}
-                            placeholder="Select a place"
                             className="w-full border p-2 rounded-lg"
                             isSearchable
                         />
+                        {placeDetails.length > 0 && (
+                            <Select
+                                options={placeDetails.map(detail => ({ value: detail.placeDetailId, label: detail.postalCode }))}
+                                value={placeDetails.find(detail => detail.value === postalCode)}
+                                onChange={(selectedOption) => {
+                                    setPostalCode(selectedOption.label);
+                                    setPlaceDetailId(selectedOption.value);
+                                }}
+                                className="w-full border p-2 rounded-lg mt-2"
+                                isSearchable
+                            />
+                        )}
                     </div>
                 )}
 
-                {!useCurrentLocation && placeDetails && placeDetails.length > 0 && (
+                {locationMode === "community" && (
                     <div>
-                        <label className="block font-bold mb-1">Postal Code:</label>
+                        <label className="block font-bold mb-1">Select From Your Communities:</label>
                         <Select
-                            options={placeDetails.map((detail) => ({ value: detail.placeDetailId, label: detail.postalCode }))}
-                            value={placeDetails.find((detail) => detail.value === postalCode)}
+                            options={userCommunities.map(comm => ({
+                                value: comm.placeDetailId,
+                                label: `${comm.placeDetail.place.placeName} - ${comm.placeDetail.postalCode}`,
+                            }))}
+                            value={userCommunities.find(option => option.value === placeDetailId)}
                             onChange={(selectedOption) => {
                                 setPlaceDetailId(selectedOption.value);
-                                setPostalCode(selectedOption.label)
-                            }
-                            }
-                            placeholder="Select a postal code"
+                            }}
                             className="w-full border p-2 rounded-lg"
                             isSearchable
                         />
                     </div>
                 )}
-
-                {useCurrentLocation && <button
-                    type="button"
-                    onClick={fetchUserLocation}
-                    className="flex items-center justify-center bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600 transition-all duration-200 disabled:opacity-50"
-                    disabled={isRefreshing}
-                >
-                    <span>Refresh Location</span>
-                    {isRefreshing ? (<div
-                        className="inline-block ml-2 h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                        role="status">
-                        <span
-                            className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-                        >Loading...</span>
-                    </div>) :
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="size-4 ml-2"
-                        >
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
-                        </svg>}
-                </button>}
 
                 <div>
                     <label className="block font-bold mb-1">Image:</label>
